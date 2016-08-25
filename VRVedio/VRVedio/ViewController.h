dbdced0ce25eed47cd004e7d46601787f2724f55
1361579531@qq.com <1361579531@qq.com>
2016-08-25 17:05:21 +0800

    init

---
+++ VRVedio/VRVedio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VRVedio
+//
+//  Created by Linyou-IOS-1 on 16/8/25.
+//  Copyright © 2016年 TGSDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
